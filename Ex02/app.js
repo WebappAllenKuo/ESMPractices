692ceb790109473f3ccfed670861392e0f3825e8
allen <g01.webapp@gmail.com>
2024-04-16 17:09:40 +0800

    export 可以多個,但 import 可以只挑自己需要即可,不必全部

--- Ex02/app.js
+++ Ex02/app.js
@@ -1,4 +1,3 @@
-import { pi as piValue, add as funcAdd } from "./math.js";
+import { add } from "./math.js";
 
-console.log("piValue=", piValue);
-console.log("funcAdd(2, 6)=", funcAdd(2, 6)); // 8
+console.log("add(2, 7)=", add(2, 7)); // 9
